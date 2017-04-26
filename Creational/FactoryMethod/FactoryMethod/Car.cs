23074a2b66ade8b4da681e4965c81ff6f2cf607a
rajachandra-thatipelli <rajachandra.thatipelli@gmail.com>
2017-04-26 17:22:42 +0530

    Add files via upload

---
+++ Creational/FactoryMethod/FactoryMethod/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class NanoCar : ICar
+    {
+        public string GetSpecifications()
+        {
+            Console.WriteLine("This is Nano Car");
+            return "NanoCar";
+        }
+    }
+
+    public class FigoCar : ICar
+    {
+        public string GetSpecifications()
+        {
+            Console.WriteLine("This is Figo Car");
+            return "FigoCar";
+        }
+    }                                   
+
+    public class I10Car : ICar
+    {
+        public string GetSpecifications()
+        {
+            Console.WriteLine("This is I10 Car");
+            return "I10Car";
+        }
+    }
+}
